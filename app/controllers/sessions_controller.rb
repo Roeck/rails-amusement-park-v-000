44a92a7ec0172885ec7b3972412d9078b85f8d76
Rony B <rons9n@gmail.com>
2021-01-12 21:26:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,21 @@
 class SessionsController < ApplicationController
 
-	def new
+  def sign_in
     @user = User.new
   end
 
   def create
-  	@user = User.find_by(:name => params[:user][:name])
-  		if @user && @user.authenticate(params[:user][:password])
-		    session[:user_id] = @user.id
-       	redirect_to user_path(@user)
-      else
-       	redirect_to '/'
-      end
-	end
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'sign_in'
+    end
+  end
 
-	def destroy
-		session[:user_id] = nil
-		redirect_to '/'
-	end
-
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
 end
