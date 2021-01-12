2247e480d8f1eb96eae514d8e3bab18137b2cb42
Rony B <rons9n@gmail.com>
2021-01-12 22:21:06 +0000

    Automatically backed up by Learn

--- db/migrate/20210112222056_add_admin_to_users2.rb
+++ db/migrate/20210112222056_add_admin_to_users2.rb
@@ -1,4 +1,5 @@
-class AddAdminToUsers2 < ActiveRecord::Migration[5.2]
+class AddAdminToUsers < ActiveRecord::Migration[5.1]
   def change
+    add_column :users, :admin, :boolean, default: false
   end
 end
