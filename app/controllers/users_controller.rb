2a49fefdbb09c88db2aacc731e8e61ac4d61e207
Rony B <rons9n@gmail.com>
2021-01-12 21:34:49 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,37 @@
-class UsersController < ApplicationController
-  before_action :athenticate_user, only:[:show]
-  def new
+class UsersController < ApplicationController 
+  before_action :logged_in?, only: [:show]
+  
+  def index
+    @users = User.all 
+  end 
+  
+  def new 
     @user = User.new
   end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user
-    else
-      redirect_to new_user_path
-    end
-  end
-
+  
   def show
-    if logged_in?
-      @user = User.find_by(id: params[:id])
-    else
+    @user = User.find_by_id(params[:id])
+    
+    if !logged_in? 
       redirect_to root_path
     end
   end
+  
+  def create 
+    @user = User.create(user_params)
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      render :new
+    end 
+  end 
+  
 
-  private
-
+  
+  private 
+  
   def user_params
-    params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets, :admin)
+    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
   end
-end
+end 
