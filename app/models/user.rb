c20ceecde81cade9fced2eb9878ca40ea34e8336
Rony B <rons9n@gmail.com>
2021-01-12 21:38:14 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  
   has_many :rides
   has_many :attractions, through: :rides
 
+  validates :name, presence: true
+
+  has_secure_password
+  
   def mood
-    self.happiness.to_i >= self.nausea.to_i ? "happy" : "sad"
+    if self.happiness.nil? || self.nausea.nil?
+      "Okay"        
+    elsif self.happiness >= self.nausea
+      "happy"
+    else
+      "sad"
+    end
   end
+  
 end
