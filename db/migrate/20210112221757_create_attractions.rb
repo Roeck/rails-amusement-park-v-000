5be2582372a423991edda7acaec3411bc40fc211
Rony B <rons9n@gmail.com>
2021-01-12 22:18:47 +0000

    Automatically backed up by Learn

--- db/migrate/20210112221757_create_attractions.rb
+++ db/migrate/20210112221757_create_attractions.rb
@@ -1,6 +1,11 @@
-class CreateAttractions2 < ActiveRecord::Migration[5.2]
+class CreateAttractions < ActiveRecord::Migration[5.1]
   def change
-    create_table :attractions2s do |t|
+    create_table :attractions do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
     end
   end
 end
